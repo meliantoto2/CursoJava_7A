025ff2cd93d778a041cf04784fc70f5efd82a8b9
ulises1525 <ulisescaballeropaez2906@gmail.com>
2023-06-16 16:09:11 -0300

    PrimerEjercicio

---
+++ modulo4/ejercicio1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class EvaluacionesAlumno {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese la primera evaluación:");
+        int evaluacion1 = scanner.nextInt();
+
+        System.out.println("Ingrese la segunda evaluación:");
+        int evaluacion2 = scanner.nextInt();
+
+        System.out.println("Ingrese la tercera evaluación:");
+        int evaluacion3 = scanner.nextInt();
+
+        double promedio = (evaluacion1 + evaluacion2 + evaluacion3) / 3.0;
+
+        if (promedio >= 7) {
+            System.out.println("El alumno ha aprobado con un promedio de " + promedio);
+        } else {
+            System.out.println("El alumno ha reprobado con un promedio de " + promedio);
+        }
+    }
+}
+
