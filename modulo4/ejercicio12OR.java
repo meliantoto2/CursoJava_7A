7e941d8673b75b3b408fcdb68fbbc7032d2cbdac
meliantoto2 <107575377+meliantoto2@users.noreply.github.com>
2023-06-28 21:57:29 -0300

    Update ejercicio12OR.java

--- modulo4/ejercicio12OR.java
+++ modulo4/ejercicio12OR.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DeterminarDocenaOr {
+public class ejercicio12OR {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
