061f54dffac398e741cf3a0c43c05996ca4a6d8d
meliantoto2 <107575377+meliantoto2@users.noreply.github.com>
2023-06-28 22:00:21 -0300

    Update ejercicio21.java

--- modulo4/ejercicio21.java
+++ modulo4/ejercicio21.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SueldoNetoEmpleado {
+public class ejercicio21 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
