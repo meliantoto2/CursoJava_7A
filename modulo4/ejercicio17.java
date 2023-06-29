18bf9c2bbd21c0cbecaab7a8cde07fd522c8c3f2
meliantoto2 <107575377+meliantoto2@users.noreply.github.com>
2023-06-28 21:58:54 -0300

    Update ejercicio17.java

--- modulo4/ejercicio17.java
+++ modulo4/ejercicio17.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class EvaluacionAlumno {
+public class ejercicio17 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
