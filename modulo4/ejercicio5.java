4f693ff0156bc8b19f708e52630156783d08cbee
meliantoto2 <107575377+meliantoto2@users.noreply.github.com>
2023-06-28 22:01:06 -0300

    Update ejercicio5.java

--- modulo4/ejercicio5.java
+++ modulo4/ejercicio5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MedallasTorneo {
+public class ejercicio5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
