e9c295bef316d0b219d97a90eac26c4168b29bc6
meliantoto2 <107575377+meliantoto2@users.noreply.github.com>
2023-06-28 21:59:49 -0300

    Update ejercicio19.java

--- modulo4/ejercicio19.java
+++ modulo4/ejercicio19.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public class NumerosAleatorios {
+public class ejercicio19 {
     public static void main(String[] args) {
         Random random = new Random();
         int contador = 0;
