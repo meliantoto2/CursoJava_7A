43ccd74c27abc5be12f8ea61cdc430311ccf40c0
meliantoto2 <107575377+meliantoto2@users.noreply.github.com>
2023-06-28 21:57:15 -0300

    Update ejercicio12AND.java

--- modulo4/ejercicio12AND.java
+++ modulo4/ejercicio12AND.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DeterminarDocenaAnd {
+public class ejercicio12AND {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
