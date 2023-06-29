6ec2babfafd8c3beecf438f15a5c400cb7b2f125
meliantoto2 <107575377+meliantoto2@users.noreply.github.com>
2023-06-28 21:59:36 -0300

    Update ejercicio18.java

--- modulo4/ejercicio18.java
+++ modulo4/ejercicio18.java
@@ -1,4 +1,4 @@
-public class TablasMultiplicar {
+public class ejercicio18 {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             System.out.println("Tabla de multiplicar del " + i + ":");
