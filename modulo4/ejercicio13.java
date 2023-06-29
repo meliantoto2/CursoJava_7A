7c1f7314280748d7f462521e095c85c5cccd43a7
meliantoto2 <107575377+meliantoto2@users.noreply.github.com>
2023-06-28 21:57:43 -0300

    Update ejercicio13.java

--- modulo4/ejercicio13.java
+++ modulo4/ejercicio13.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DeterminarDiasMesSwitch {
+public class ejercicio13 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
